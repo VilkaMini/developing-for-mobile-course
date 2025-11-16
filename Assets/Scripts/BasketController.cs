86129286d3aecf24392f7ee7ac35fc3776ec4015
Vilius <43584156+VilkaMini@users.noreply.github.com>
2025-11-16 20:06:34 +0100

    Added background and acceleration basket movement

--- Assets/Scripts/BasketController.cs
+++ Assets/Scripts/BasketController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,7 @@ public class BasketController : MonoBehaviour
     // Movement
     private InputAction m_moveAction;
     private Vector2 m_moveAmt;
+    private float m_accelerationX;
 
     // Managers
     [SerializeField]
@@ -22,12 +24,26 @@ public class BasketController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Set initial values
+        Vector3 newPos = transform.position;
         m_moveAmt = m_moveAction.ReadValue<Vector2>();
+        m_accelerationX = Input.acceleration.x;
+
+
+        // Do the magic (movement)
+        if (Mathf.Abs(m_accelerationX) > 0.1) // 0.1 here is a cutoff so that the basket would not drift
+        {
+            newPos = transform.position + new Vector3(m_accelerationX * speed * Time.deltaTime, 0, 0);
+        }
 
         if (m_moveAction.IsPressed())
         {
-            transform.position += new Vector3(m_moveAmt[0] * speed * Time.deltaTime, 0, 0);
+            newPos = transform.position +  new Vector3(m_moveAmt[0] * speed * Time.deltaTime, 0, 0);
         }
+
+        newPos.x = Mathf.Clamp(newPos.x, -2.3f, 2.3f);
+
+        transform.position = newPos;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
