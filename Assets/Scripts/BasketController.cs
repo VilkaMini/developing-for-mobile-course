53d3d1b0baeb652a3feaf8f265591a914b3f7af8
Vilius <43584156+VilkaMini@users.noreply.github.com>
2025-11-20 19:32:12 +0100

    Added touch controls for moving basket

--- Assets/Scripts/BasketController.cs
+++ Assets/Scripts/BasketController.cs
@@ -1,6 +1,8 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using static UnityEditor.PlayerSettings;
 
 public class BasketController : MonoBehaviour
 {
@@ -12,6 +14,9 @@ public class BasketController : MonoBehaviour
     private Vector2 m_moveAmt;
     private float m_accelerationX;
 
+    private InputAction primaryTouch;
+    private InputAction primaryTouchHold;
+
     // Managers
     [SerializeField]
     private ScoringManager scoringManager;
@@ -19,6 +24,8 @@ public class BasketController : MonoBehaviour
     private void Awake()
     {
         m_moveAction = InputSystem.actions.FindAction("Move");
+        primaryTouch = InputSystem.actions.FindAction("TouchInput");
+        primaryTouchHold = InputSystem.actions.FindAction("TouchHold");
     }
 
     // Update is called once per frame
@@ -29,7 +36,19 @@ public class BasketController : MonoBehaviour
         m_moveAmt = m_moveAction.ReadValue<Vector2>();
         m_accelerationX = Input.acceleration.x;
 
-
+        //print(primaryTouch.IsPressed());
+        //print(primaryTouch.ReadValue<Vector2>());
+        /*
+        if (primaryTouch.WasPerformedThisFrame())
+        {
+            print("Perform");
+        }
+        if (primaryTouch.WasReleasedThisFrame()) {
+            print("Complete");
+        }
+        */
+        
+        
         // Do the magic (movement)
         if (Mathf.Abs(m_accelerationX) > 0.1) // 0.1 here is a cutoff so that the basket would not drift
         {
@@ -39,6 +58,13 @@ public class BasketController : MonoBehaviour
         if (m_moveAction.IsPressed())
         {
             newPos = transform.position +  new Vector3(m_moveAmt[0] * speed * Time.deltaTime, 0, 0);
+        }
+
+        if (primaryTouchHold.ReadValue<float>() > 0)
+        {
+            Vector2 touchPos = primaryTouch.ReadValue<Vector2>();
+            float direction = (touchPos.x < Screen.width * 0.5f) ? -1f : 1f;
+            newPos = transform.position + Vector3.right * direction * speed * Time.deltaTime;
         }
 
         newPos.x = Mathf.Clamp(newPos.x, -2.3f, 2.3f);
